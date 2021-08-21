fe1ed7895f0a3dad48d791fb17b4c4b2ddbf2e9a
Gary Coppeler <coppeler@onmail.com>
2021-08-21 11:03:03 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Gary Coppeler on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
